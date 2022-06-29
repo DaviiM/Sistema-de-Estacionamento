aec9f54d29b8f901a6fde470a780bdcfe8a0396a
Davi Melo <57837321+DaviiM@users.noreply.github.com>
2022-06-29 00:50:11 -0300

    Update menu.php

--- menu.php
+++ menu.php
@@ -14,8 +14,8 @@
       <li class="nav-item active">
         <a class="nav-link" href="cadcontrato.php">Cadastrar Contrato</a>
       </li>
-      <li class="nav-item">
-        <a class="nav-link disabled" href="#">Disabled</a>
+      <li class="nav-item active">
+        <a class="nav-link" href="cadcontrato.php?acao=select">Cadastrar Contrato</a>
       </li>
     </ul>
   </div>
